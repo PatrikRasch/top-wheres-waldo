77bfef81ce81340ec02a6ef850d21c61432c27ac
PatrikRasch <patrikrask@hotmail.com>
2023-07-12 14:28:14 +0200

    3xl rule added for extra large screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,14 @@ module.exports = {
         "miss-shake": "shakeNo 0.3s ease",
       },
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "2000px",
+    },
   },
   plugins: [],
 };
